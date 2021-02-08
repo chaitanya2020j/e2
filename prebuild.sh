85bef893ac406baf1c1186e374dcdce7480cca93
Debarghya Pandit <33447931+debarghyapandit@users.noreply.github.com>
2021-02-08 19:23:34 +0530

    Update prebuild.sh

--- prebuild.sh
+++ prebuild.sh
@@ -1,18 +1,15 @@
-sudo apt install lcov -y
 
-
-#ARG BUILD_TYPE="Release"
 BUILD_TYPE="Debug"
-
+cd $WORKSPACE/RIC-E2-TERMINATION
+sudo cp CMakeLists.txt ../
+cd ..
 wget https://github.com/Kitware/CMake/releases/download/v3.19.4/cmake-3.19.4-Linux-x86_64.sh
 echo "y" >  /tmp/y
 sh cmake-3.19.4-Linux-x86_64.sh < /tmp/y
-ls ..
 export PATH="/w/workspace/ric-plt-e2-cmake-sonarqube/cmake-3.19.4-Linux-x86_64/bin:$PATH"
-pwd
-ls
 sudo apt-get update
-sudo apt install lcov -y\
+sudo apt install libsctp-dev -y
+sudo apt install lcov  -y\
     && wget --content-disposition https://github.com/cgreen-devs/cgreen/releases/download/1.2.0/cgreen_1.2.0_amd64.deb \
     && sudo dpkg -i cgreen_1.2.0_amd64.deb \
     && wget --content-disposition https://packagecloud.io/o-ran-sc/release/packages/debian/stretch/rmr_4.4.6_amd64.deb/download.deb \
@@ -26,6 +23,12 @@ sudo apt install lcov -y\
     && sudo apt-get install -y libcurl4-gnutls-dev gawk libtbb-dev  libtbb-doc libtbb2 libtbb2-dbg \
     && cd RIC-E2-TERMINATION/3rdparty && git clone https://github.com/oktal/pistache.git && cd pistache \
     && sudo git submodule update --init \
-    && mkdir -p RIC-E2-TERMINATION/cmake-modules \
-    && wget https://github.com/bilke/cmake-modules/blob/master/CodeCoverage.cmake -O RIC-E2-TERMINATION/cmake-modules/CodeCoverage.cmake
-sudo ls -lR
+    && sudo ldconfig \
+    && cd $WORKSPACE/RIC-E2-TERMINATION/3rdparty && git clone -v https://github.com/jupp0r/prometheus-cpp.git \
+    && cd prometheus-cpp && git submodule init && git submodule update && mkdir build && cd build \
+    && sudo ldconfig \
+    && cd $WORKSPACE/RIC-E2-TERMINATION/3rdparty && git clone https://github.com/jarro2783/cxxopts.git \
+    && cd $WORKSPACE/RIC-E2-TERMINATION/3rdparty && git clone https://github.com/Tencent/rapidjson.git \
+    && cd $WORKSPACE/RIC-E2-TERMINATION/3rdparty && git clone https://github.com/zeux/pugixml.git \
+    && cd $WORKSPACE && git clone https://github.com/bilke/cmake-modules.git
+
